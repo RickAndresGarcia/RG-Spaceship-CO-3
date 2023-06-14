5beca9c339a6c20b79209409aadb44881a954b93
RickAndresGarcia <ricardo.garcia@fundacion-jala.org>
2023-06-14 13:46:29 -0500

    Clase 2: Tarea

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,17 +1,26 @@
+from game.components.enemies.secondenemy import SecondEnemy
 from game.components.enemies.enemy import Enemy
 
 
 class EnemyManager:
     def __init__(self):
         self.enemies: list[Enemy] = []
+        self.secondenemies: list[SecondEnemy] = []
 
     def update(self):
-        if not self.enemies:
+        if not self.enemies and not self.secondenemies:
             self.enemies.append(Enemy())
+            self.secondenemies.append(SecondEnemy())
             
         for enemy in self.enemies:
             enemy.update(self.enemies)
 
+        for senemy in self.secondenemies:
+            senemy.update(self.secondenemies)
+
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+            
+        for senemy in self.secondenemies:
+            senemy.draw(screen)
---
+++ game/components/enemies/secondenemy.py
@@ -0,0 +1,65 @@
+import random
+import pygame
+from pygame.sprite import Sprite
+from game.utils.constants import ENEMY_2, ENEMY_TYPE, SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+class SecondEnemy(Sprite):
+    X_POS_LIST = [50,100,150,200,250,300,350,400,450,500,550,500,650,700,750,800,850,900,950,1000,1050]
+    Y_POS = 30
+    SPEED_X = 3
+    SPEED_Y = 3
+    SPEED_UP = 5
+    MOVEMENTS = ["left","right", "left up", "right up"]
+
+    def __init__(self):
+        self.image = pygame.transform.scale(ENEMY_2,(50,50))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.choice(self.X_POS_LIST)
+        self.rect.y = self.Y_POS
+        self.type = ENEMY_TYPE
+        self.speed_x = self.SPEED_X
+        self.speed_y = self.SPEED_Y
+        self.speed_up = self.SPEED_UP
+        self.movement = self.MOVEMENTS
+        self.moving_index = random.randint(0, len(self.MOVEMENTS) - 1)
+
+    def update(self, ships):
+
+        self.rect.y += self.speed_y
+        movement = self.movement[self.moving_index]
+
+        match movement:
+            case "left":
+                self.rect.x -= self.speed_x
+            case "right":
+                self.rect.x += self.speed_x
+            case "left up":
+                self.rect.y -= self.speed_up
+                self.rect.x -= self.speed_x
+            case "right up":
+                self.rect.y -= self.speed_up
+                self.rect.x += self.speed_x
+
+        self.update_movement ()
+
+        if self.rect.y >= SCREEN_HEIGHT:
+            ships.remove(self)
+
+    def update_movement (self):
+        if self.rect.x <= 0:
+            self.rect.x = SCREEN_WIDTH
+        elif self.rect.x >= SCREEN_WIDTH:
+            self.rect.x = 1  
+        elif self.rect.y <= 0:
+            self.rect.y += self.speed_y
+        elif self.rect.y <= 0:
+            self.rect.y += self.speed_y
+
+        self.moving_index = random.randint(0, len(self.MOVEMENTS) - 1)
+
+        
+
+
+    def draw(self, screen):
+        screen.blit(self.image,(self.rect.x,self.rect.y))
